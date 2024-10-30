a2d6eee2c56a431edc22770c3111dfe6bd48f241
amankumargit45 <amankumargupta899@gmail.com>
2024-10-30 16:17:46 +0000

    datatype_conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,20 @@
+let score="33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueinnumber =Number(score)//here it gets converted to number altough score is not a pure number
+// console.log(  valueinnumber);
+// "33"=>33
+// "44abc"=>NaN-Not a Number
+//true=>1;false=>0
+let isloggedin=""
+let booleanisloggedin=Boolean(isloggedin)
+// console.log(booleanisloggedin);
+// "" =>false
+//"aman"=>true
+let someNumber=33
+let stringnum=String(someNumber)
+console.log(stringnum);
+console.log(typeof stringnum);
+
