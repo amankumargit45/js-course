075f74956e2d9cee4d903cff3f7b0f4f7f5edff9
amankumargit45 <amankumargupta899@gmail.com>
2024-10-30 10:48:32 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accoundId=14456
+let accountEmail="amankumargupta899@gmail.com"
+var accoutPassword="12344"
+accountCity="Patna"
+accountEmail="sbhjss@gmail.com"
+accountCity="bangalore"
+let accountState;
+// console.log(accoundId);
+// console.log(accountEmail);
+
+console.table([accoundId,accountEmail,accoutPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";  //treat all js code as newer version
+
+//alert(3+3) error bcoz we are using nodejs , not browser
+console.log(4 + 3);
+let name="aman"
+let age=18
+let isloggedin=false
+// number=> 2^53 range
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>it is a standalone value
+// undefined =>when value is not defined but is only declared
+//symbol=>uniqueness
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
+
+
