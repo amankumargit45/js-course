0d5b886535d947deabf9493d8374e790466d3492
amankumargit45 <amankumargupta899@gmail.com>
2024-10-31 08:53:52 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+//there are two types datatypes on the basis of how the data is being stored and retrieved from memory
+
+
+//1.Primitive-seven categories-(changes in copy of data)-eg:String,NUmber,Boolean,null,undefined,Symbol
+//BigInt
+
+//javascript is dynamically typed language
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid);//false not of same type
+
+
+//2.Non primitive/reference types-Arrays,objects,functions 
+//return type of non primitive datatype is always an object
+
+const heroes=["aman","doga","naagraj"];//array
+//object always within curley braces
+let myobj={
+    name:"aman",
+    age:"22",
+}
+
+
+const myfunction=function(){
+    console.log("hello wo0rld");
+}
+
+console.log();
